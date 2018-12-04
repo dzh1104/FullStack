c78cab98e4ba5dc8214a378dded04d7aa150f7bc
dzh1104 <zhding1104@163.com>
2018-12-04 22:50:49 +0800

    单例类

---
+++ src/main/java/com/github/fight/ObjectOrientedDown/Singleton.java
@@ -0,0 +1,13 @@
+package com.github.fight.ObjectOrientedDown;
+
+// 单例类
+
+/*
+* 该类只能创建一个对象，构造器使用private修饰符
+* */
+public class Singleton {
+
+    public static void main(String[] args) {
+
+    }
+}
