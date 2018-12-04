899ed59d83f0e08baf21588d176f324b2d0c25b3
dzh1104 <zhding1104@163.com>
2018-12-04 22:58:21 +0800

    单例类demo

--- src/main/java/com/github/fight/ObjectOrientedDown/Singleton.java
+++ src/main/java/com/github/fight/ObjectOrientedDown/Singleton.java
@@ -5,9 +5,23 @@ package com.github.fight.ObjectOrientedDown;
 /*
 * 该类只能创建一个对象，构造器使用private修饰符
 * */
+
+class Test {
+    private static Test instance;
+    private Test () {}
+
+    public static Test getInstance() {
+        if (Test.instance == null) {
+            Test.instance = new Test();
+        }
+        return Test.instance;
+    }
+}
 public class Singleton {
 
     public static void main(String[] args) {
-
+        Test t1 = Test.getInstance();
+        Test t2 = Test.getInstance();
+        System.out.println(t1 == t2);
     }
 }
